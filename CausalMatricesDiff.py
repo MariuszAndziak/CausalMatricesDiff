d4eeabfe13e2c6d66bf9e0231b51408217205c0a
MariuszAndziak <43908826+MariuszAndziak@users.noreply.github.com>
2024-12-02 21:32:54 +0100

    Added text description to causal differences

--- CausalMatricesDiff.py
+++ CausalMatricesDiff.py
@@ -10,27 +10,29 @@ class CausalMatricesDiff(object):
     '''
     Visualize differences between two causal matrices. 
     '''
-    def __init__(self, true_dag, pred_dag):
+    def __init__(self, true_dag, pred_dag, var_names = None):
         self.true_dag = true_dag
         self.pred_dag = pred_dag
-    
+        self.fn_list, self.fp_list = self.get_not_equal()
+        self.var_names = var_names
 
-    def causal_matrices_diff(self, var_names = None, save_name = None, figsize=(12,6), cmap_name='Greys'):
+        if self.var_names == None:
+            self.var_names = list(range(self.true_dag.shape[0]))
 
-        def get_not_equal(self):
-            result_fn = []; result_fp = []
-            for i in range(self.pred_dag.shape[0]):
-                for j in range(self.pred_dag.shape[0]):
-                    if (self.pred_dag[i,j] == 0) and (self.true_dag[i,j] == 1):
-                        result_fn.append([i,j])
-                    if (self.pred_dag[i,j] == 1) and (self.true_dag[i,j] == 0):
-                        result_fp.append([i,j])
-            return result_fn, result_fp
-        
-        fn_list, fp_list = get_not_equal(self)
-        
-        if var_names == None:
-            var_names = list(range(self.true_dag.shape[0]))
+    def get_not_equal(self):
+        result_fn = []; result_fp = []
+        for i in range(self.pred_dag.shape[0]):
+            for j in range(self.pred_dag.shape[0]):
+                if (self.pred_dag[i,j] == 0) and (self.true_dag[i,j] == 1):
+                    result_fn.append([i,j])
+                if (self.pred_dag[i,j] == 1) and (self.true_dag[i,j] == 0):
+                    result_fp.append([i,j])
+        return result_fn, result_fp
+
+    def causal_matrices_diff(self, save_name = None, figsize=(12,6), cmap_name='Greys'):
+
+        # if var_names == None:
+        #     var_names = list(range(self.true_dag.shape[0]))
 
         assert self.true_dag.shape[0] == len(var_names), "Length of variable list doesn't match the shape of the causal matrix"
 
@@ -38,17 +40,17 @@ class CausalMatricesDiff(object):
         for n in range(2):
 
             sns.heatmap(self.true_dag, cmap=cmap_name, ax = ax[n], cbar=False, linewidths=0, linecolor='k', facecolor='white', edgecolor='k');
-            ax[n].set_xticklabels(var_names, rotation=90);
-            ax[n].set_yticklabels(var_names, rotation=0);
+            ax[n].set_xticklabels(self.var_names, rotation=90);
+            ax[n].set_yticklabels(self.var_names, rotation=0);
             ax[n].set_aspect('equal');
 
         ax[0].set_title('True DAG')
         ax[1].set_title('Pred DAG')
 
-        for j,i in fp_list:
+        for j,i in self.fp_list:
             ax[1].add_patch(Rectangle((i, j), 1, 1, linewidth=0, facecolor='#D3D3D3', edgecolor='k'))
 
-        for j,i in fn_list:
+        for j,i in self.fn_list:
             ax[1].add_patch(Rectangle((i, j), 1, 1, linewidth=2, facecolor = 'white', edgecolor='k'))
         
         if save_name is not None:
@@ -73,6 +75,27 @@ class CausalMatricesDiff(object):
     def structural_humming_distance(self):
         diff = abs(self.true_dag - self.pred_dag)
         return np.sum(diff)
+    
+    def explain_differences(self, return_text_description = True):
+        false_negatives_variables  = [[self.var_names[x], self.var_names[y]] for x, y in self.fn_list]
+        false_positives_variables  = [[self.var_names[x], self.var_names[y]] for x, y in self.fp_list]
+
+        if return_text_description:
+            print("Pred DAG doesn't have causal paths from:")
+            for elem in false_negatives_variables:
+                print(f'- {elem[0]} to {elem[1]}')
+            print('which are present in True DAG')
+            print('-'*30)
+            print("Pred DAG has addiational causal paths from:")
+            for elem in false_positives_variables:
+                print(f'- {elem[0]} to {elem[1]}')
+            print('which are not present in True DAG')
+        else:
+            return {
+                'False Negatives': false_negatives_variables,
+                'False Positives': false_positives_variables
+            }
+        
 
 true_dag = np.array([[0., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
        [1., 0., 0., 0., 0., 0., 0., 0., 0., 0.],
@@ -97,7 +120,8 @@ pred_dag = np.array([[0., 0., 0., 0., 0., 0., 0., 1., 0., 0.],
        [0., 1., 0., 0., 1., 0., 0., 0., 1., 0.]])
 
 var_names = [f'variable_{n}' for n in range(10)]
-matrices = CausalMatricesDiff(true_dag, pred_dag)
-matrices.causal_matrices_diff(save_name='example.png', var_names=var_names)
+matrices = CausalMatricesDiff(true_dag, pred_dag, var_names=var_names)
+matrices.causal_matrices_diff(save_name='example.png')
 print(matrices.number_of_undirected())
 print(matrices.structural_humming_distance())
+print(matrices.explain_differences(return_text_description=True))
